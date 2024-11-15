aecf308eb503cdf297ee93df9d50790f63272f09
huojichuanqi <76831049+huojichuanqi@users.noreply.github.com>
2024-11-15 17:22:32 +0800

    atr跟振幅定价差

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # jiezhen
 接针测试
+
+config_bak.json  改成config.json
--- zhen.py
+++ zhen.py
@@ -4,25 +4,29 @@ import okx.Public_api as PublicAPI
 import okx.Market_api as MarketAPI
 import json
 import logging
+import requests
 from logging.handlers import TimedRotatingFileHandler
 
+# 读取配置文件
 with open('config.json', 'r') as f:
     config = json.load(f)
-# 提取OKX部分的配置
-okx_config = config['okx']
 
-# 配置 OKX 第三方库
+# 提取配置
+okx_config = config['okx']
+trading_pairs_config = config.get('tradingPairs', {})
+monitor_interval = config.get('monitor_interval', 60)  # 默认60秒
+feishu_webhook = config.get('feishu_webhook', '')
+
 trade_api = TradeAPI.TradeAPI(okx_config["apiKey"], okx_config["secret"], okx_config["password"], False, '0')
 market_api = MarketAPI.MarketAPI(okx_config["apiKey"], okx_config["secret"], okx_config["password"], False, '0')
 public_api = PublicAPI.PublicAPI(okx_config["apiKey"], okx_config["secret"], okx_config["password"], False, '0')
-# 配置日志
+
 log_file = "log/okx.log"
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-# 使用 TimedRotatingFileHandler 以天为单位进行日志分割
 file_handler = TimedRotatingFileHandler(log_file, when='midnight', interval=1, backupCount=7, encoding='utf-8')
-file_handler.suffix = "%Y-%m-%d"  # 设置日志文件名的后缀格式，例如 multi_asset_bot.log.2024-11-05
+file_handler.suffix = "%Y-%m-%d"
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
@@ -31,72 +35,141 @@ console_handler = logging.StreamHandler()
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 
-logger = logger
 
-# 获取标记价格
+def send_feishu_notification(message):
+    if feishu_webhook:
+        headers = {'Content-Type': 'application/json'}
+        data = {"msg_type": "text", "content": {"text": message}}
+        response = requests.post(feishu_webhook, headers=headers, json=data)
+        if response.status_code == 200:
+            print("飞书通知发送成功")
+        else:
+            print(f"飞书通知发送失败: {response.text}")
+    else:
+        print("飞书Webhook URL未配置")
+
+
 def get_mark_price(instId):
-    # 这里需要调用一个获取标记价格的API接口
     response = market_api.get_ticker(instId)
-
     if 'data' in response and len(response['data']) > 0:
-        # 第一条数据中的 'last' 字段
         last_price = response['data'][0]['last']
         return float(last_price)
     else:
         raise ValueError("Unexpected response structure or missing 'last' key")
 
 
+def get_historical_klines(instId, bar='1m', limit=30):
+    response = market_api.get_candlesticks(instId, bar=bar, limit=limit)
+    if 'data' in response and len(response['data']) > 0:
+        # Return the Kline data which includes [timestamp, open, high, low, close, volume]
+        return response['data']
+    else:
+        raise ValueError("Unexpected response structure or missing candlestick data")
+
+
+def calculate_atr(klines, period=14):
+    trs = []
+    for i in range(1, len(klines)):
+        high = float(klines[i][2])
+        low = float(klines[i][3])
+        prev_close = float(klines[i-1][4])
+        tr = max(high - low, abs(high - prev_close), abs(low - prev_close))
+        trs.append(tr)
+    atr = sum(trs[-period:]) / period
+    return atr
+
+
+def calculate_average_amplitude(klines, period=14):
+    amplitudes = []
+    for i in range(len(klines) - period, len(klines)):
+        high = float(klines[i][2])
+        low = float(klines[i][3])
+        close = float(klines[i][4])  # 使用收盘价作为基准
+        amplitude = ((high - low) / close) * 100  # 计算高低价差的百分比
+        amplitudes.append(amplitude)
+    average_amplitude = sum(amplitudes) / len(amplitudes)
+    return average_amplitude
+
+
+
 def cancel_all_orders(instId):
-    # 获取当前挂单列表
     open_orders = trade_api.get_order_list(instId=instId, state='live')
     order_ids = [order['ordId'] for order in open_orders['data']]
-
-    # 取消所有挂单
     for ord_id in order_ids:
         trade_api.cancel_order(instId=instId, ordId=ord_id)
-    print("挂单取消成功.")
+    logger.info(f"{instId}挂单取消成功.")
 
 
-def place_order(instId, price):
-    amount_usdt = 20  # 下单金额，1x杠杆算
+def place_order(instId, price, amount_usdt):
     response = public_api.convert_contract_coin(type='1', instId=instId, sz=str(amount_usdt), px=str(price),
                                                 unit='usdt', opType='open')
-
     if response['code'] == '0':
         sz = response['data'][0]['sz']
-
         if float(sz) > 0:
-            # 下一个限价买单
             order_result = trade_api.place_order(
                 instId=instId,
-                tdMode='cash',  # 请根据实际情况设置交易模式
+                tdMode='isolated',
                 side='buy',
                 ordType='limit',
-                sz=sz,  # 使用转换后的张数
-                px=str(price)  # 价格需为字符串格式
+                sz=sz,
+                px=str(price)
             )
-            print(f"下单成功: {order_result}")
+            print(f"{instId}下单结果: {order_result}")
+
+            # send_feishu_notification(f"{instId}下单结果: {order_result}")
         else:
-            print("计算出的合约张数太小，无法下单。")
+            logger.info(f"{instId}计算出的合约张数太小，无法下单。")
+            # send_feishu_notification(f"{instId}计算出的合约张数太小，无法下单。")
     else:
-        print(f"转换失败: {response['msg']}")
+        logger.info(f"{instId}转换失败: {response['msg']}")
+        send_feishu_notification(f"{instId}转换失败: {response['msg']}")
 
 
 def main():
-    instId = 'CTC-USDT-SWAP'  # 设置交易对
     while True:
         try:
-            mark_price = get_mark_price(instId)
-            target_price = mark_price * 0.99
+            for instId, pair_config in trading_pairs_config.items():
+                mark_price = get_mark_price(instId)
+                klines = get_historical_klines(instId)
+                atr = calculate_atr(klines)
+                # print(klines)
 
-            cancel_all_orders(instId)
-            place_order(instId, target_price)
+                # 打印ATR和当前价格/ATR比值
+                price_atr_ratio = (mark_price / atr) / 100
+                # print(atr)
+                # print(price_atr_ratio)
+                # continue
+                logger.info(f"{instId} ATR: {atr}, 当前价格/ATR比值: {price_atr_ratio:.3f}")
 
-            time.sleep(60)  # 每60秒运行一次
+                average_amplitude = calculate_average_amplitude(klines)
+
+                logger.info(f"{instId} ATR: {atr}, 平均振幅: {average_amplitude:.2f}%")
+
+                # 选择较小的值，并确保最终值不小于0.005
+                selected_value = min(average_amplitude, price_atr_ratio)
+
+                selected_value = max(selected_value, 0.5)
+
+                # price_factor = pair_config.get('price_factor', 0.99)  # 默认值0.99
+                # price_factor = 1 - price_atr_ratio  # 接针价差
+                price_factor = 1 - selected_value / 100  # 确保price_factor不为负
+
+
+                amount_usdt = pair_config.get('amount_usdt', 20)  # 默认值20
+                target_price = mark_price * price_factor
+                logger.info(f"{instId} ATR: {atr}, 当前挂单价差: {price_factor:.3f}，价格: {target_price:.6f}")
+
+
+
+                cancel_all_orders(instId)
+                place_order(instId, target_price, amount_usdt)
+
+            time.sleep(monitor_interval)
         except Exception as e:
-            print(f'An error occurred: {e}')
-            time.sleep(60)  # 若发生错误，等待60秒后重试
-
+            error_message = f'An error occurred: {e}'
+            logger.error(error_message)
+            send_feishu_notification(error_message)
+            time.sleep(monitor_interval)
 
 if __name__ == '__main__':
     main()
