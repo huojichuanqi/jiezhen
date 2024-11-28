24f82503ff7e509a61d3fc95f81a269531869852
PrinceFPF <promising.teenager@gmail.com>
2024-11-29 00:26:52 +0800

    Update Market_api.py
    构造函数中增加了proxies参数

--- okx/Market_api.py
+++ okx/Market_api.py
@@ -4,8 +4,8 @@ from .consts import *
 
 class MarketAPI(Client):
 
-    def __init__(self, api_key, api_secret_key, passphrase, use_server_time=False, flag='1'):
-        Client.__init__(self, api_key, api_secret_key, passphrase, use_server_time, flag)
+    def __init__(self, api_key, api_secret_key, passphrase, use_server_time=False, flag='1', proxies=None):
+        Client.__init__(self, api_key, api_secret_key, passphrase, use_server_time, flag, proxies=proxies)
 
     # Get Tickers
     def get_tickers(self, instType, uly='',instFamily=''):
